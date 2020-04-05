2d6ffc0c0a731b40e5941759ded18b1ccddec3ac
teabrewer <teabrewer@teabrewer.com>
2020-04-05 12:23:40 -0400

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # Ensure you have defined root_url to *something* in your config/routes.rb.
   resources :films
   get 'films/show'
   get 'films/title'
   get 'films/synopsis'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'films#index'
 end
