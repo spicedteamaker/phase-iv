e859b1a8da7c1bb7f007670b671dbee7794fa6fa
teabrewer <teabrewer@teabrewer.com>
2020-04-26 10:59:08 -0400

    view each user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @rentals = @user.rentals
+  end
 end
